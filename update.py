1eb01813008ca1a3734524ad16e6e9cacb71d334
mjwebhacks <98449007+mjwebhacks@users.noreply.github.com>
2022-08-04 15:31:34 +0530

    Update update.py

--- update.py
+++ update.py
@@ -57,7 +57,7 @@ update = srun([f"git init -q \
                  && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
 
 if update.returncode == 0:
-    log_info('Successfully updated with latest commit from UPSTREAM_REPO')
+    log_info('Successfully updated with latest commit from UPSTREAM_REPO MJWebHack')
 else:
     log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
 
